741f7e458fb858dc91104519bf1ce8b5853c11f6
mccloskeybr <brendan.mccloskey@gmail.com>
2021-04-15 23:07:46 -0400

    install and README

---
+++ README.md
@@ -0,0 +1,5 @@
+my dotfiles, should be good for both linux and OSX. just run the install shell script and should be good to go.
+
+```
+wget https://github.com/mccloskeybr/dotfiles/install.sh | sh
+```
---
+++ install.sh
@@ -0,0 +1,24 @@
+echo "beginning install..."
+
+# install zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+# install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install this :)
+git clone https://github.com/mccloskeybr/dotfiles ~/.dotfiles
+
+mkdir ~/.vim
+mkdir ~/.zsh
+mkdir ~/.iterm2
+
+cd ~/.dotfiles
+cp ./zshrc ~/.zshrc
+cp ./vimrc ~/.vimrc
+cp ./hgrc ~/.hgrc
+cp ./vim/* ~/.vim
+cp ./zsh/* ~/.zsh
+cp ./iterm2/* ~/.iterm2
+
+echo "done."
