41aba276d78da678b4eece3af1874271e7d84ba5
mccloskeybr <brendan.mccloskey@gmail.com>
2022-07-05 17:02:31 -0400

    try using telescope for lsp things for a little bit

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,4 @@ nnoremap("<Leader>dN", "lua vim.diagnostic.goto_prev()")
 nnoremap("<Leader>dn", "lua vim.diagnostic.goto_next()")
 
 nnoremap("<Leader>lc", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-nnoremap("<Leader>lh", "<cmd>lua vim.lsp.buf.hover()<CR>")
-nnoremap("<Leader>ld", "<cmd>lua vim.lsp.buf.definition()<CR>")
-nnoremap("<Leader>lD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-nnoremap("<Leader>jr", "<cmd>lua vim.lsp.buf.references()<CR>")
+nnoremap("<C-k>", "<cmd>lua vim.lsp.buf.hover()<CR>")
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -23,6 +23,9 @@ require('telescope').setup({
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('ui-select')
 
-nnoremap("<Leader>tf", "<cmd>lua require(\"telescope.builtin\").find_files({ cwd = get_project_root() })<CR>")
+nnoremap("<Leader>tf", "<cmd>lua require(\"telescope.builtin\").find_files({ cwd = get_project_root(), previewer = false })<CR>")
 nnoremap("<Leader>tl", "<cmd>lua require(\"telescope.builtin\").live_grep({ cwd = get_project_root() })<CR>")
 vnoremap("<Leader>tl", "<cmd>lua require(\"telescope.builtin\").live_grep({ cwd = get_project_root(), default_text = get_visual_selection() })<CR>")
+
+nnoremap("<Leader>tr", "<cmd>lua require(\"telescope.builtin\").lsp_references()<CR>")
+nnoremap("<Leader>td", "<cmd>lua require(\"telescope.builtin\").lsp_definitions()<CR>")
