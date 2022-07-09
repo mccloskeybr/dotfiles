467b6d1deaf01ae66706e820ce5f61c82ce2fddb
mccloskeybr <brendan.mccloskey@gmail.com>
2022-07-09 00:28:48 -0400

    remove debug line

--- nvim/lua/funcs/helpers.lua
+++ nvim/lua/funcs/helpers.lua
@@ -1,7 +1,6 @@
 -- https://www.reddit.com/r/neovim/comments/oo97pq/how_to_get_the_visual_selection_range/
 function get_visual_selection()
   vim.cmd('noau normal! "vy"')
-  print(vim.fn.getreg('v'))
   return vim.fn.getreg('v')
 end
 
