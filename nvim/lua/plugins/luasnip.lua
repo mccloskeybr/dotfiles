be2f4661f892f17256219799a77235db79b3d863
mccloskeybr <brendan.mccloskey@gmail.com>
2022-07-13 15:36:47 -0400

    add keybinds for luasnip

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,3 +1,5 @@
+require('funcs.map_funcs')
+
 require('packer').use({
   'L3MON4D3/LuaSnip',
   requires = {
@@ -5,5 +7,11 @@ require('packer').use({
   },
   config = function()
     require('luasnip.loaders.from_vscode').lazy_load()
+
+    inoremap_silent('<Tab>', '<cmd>lua require(\'luasnip\').jump(1)<CR>')
+    inoremap_silent('<S-Tab>', '<cmd>lua require(\'luasnip\').jump(-1)<CR>')
+
+    inoremap_silent('<C-e>', '<cmd>lua require(\'luasnip\').change_choice(1)<CR>')
+    inoremap_silent('<C-E>', '<cmd>lua require(\'luasnip\').change_choice(-1)<CR>')
   end
 })
