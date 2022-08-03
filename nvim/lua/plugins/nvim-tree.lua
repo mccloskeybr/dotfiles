6873c15c359d0d396e40cf36301ebc3911f2687c
mccloskeybr <brendan.mccloskey@gmail.com>
2022-08-03 17:01:43 -0400

    customize nvim tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,7 +6,16 @@ require('packer').use({
     'kyazdani42/nvim-web-devicons'
   },
   config = function()
-    require('nvim-tree').setup({})
+    require('nvim-tree').setup({
+      view = {
+        width = 100,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      },
+    })
 
     nnoremap('<Leader>f', '<cmd>:NvimTreeFindFile<CR>')
   end
