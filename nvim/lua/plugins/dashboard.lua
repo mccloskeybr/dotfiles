e7e0092c0958cb53a07a0f2c04d183c9cdacc32f
mccloskeybr <brendan.mccloskey@gmail.com>
2022-12-16 14:51:40 -0500

    customize dashboard / make it feel a little more homely

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,4 +1,6 @@
-banner = {
+-- DEFAULT
+dashboard_header = {
+  os.date('< %m-%d-%y  ::  %H:%M:%S >'), '',
   ' _____     ______     ______   ______     __  __     __         ______  ',
   '/\\  __-.  /\\  ___\\   /\\  ___\\ /\\  __ \\   /\\ \\/\\ \\   /\\ \\       /\\__  _\\ ',
   '\\ \\ \\/\\ \\ \\ \\  __\\   \\ \\  __\\ \\ \\  __ \\  \\ \\ \\_\\ \\  \\ \\ \\____  \\/_/\\ \\/ ',
@@ -9,6 +11,20 @@ banner = {
 require('packer').use({
   'glepnir/dashboard-nvim',
   config = function()
-    require('dashboard').custom_header = banner
+    local db = require('dashboard')
+    db.custom_header = dashboard_header
+    db.custom_center = {
+      { desc = 'Find file', action = 'Telescope find_files' },
+      { desc = 'Live grep', action = 'Telescope live_grep' },
+    }
+    db.custom_footer = { 'Number of plugins: ' .. #vim.tbl_keys(packer_plugins) }
+
+    vim.cmd('hi DashboardHeader ctermfg=167')
+    vim.cmd('hi DashboardCenter ctermfg=grey')
+    vim.cmd('hi DashboardFooter ctermfg=239')
+
+    db.header_pad = 20
+    db.center_pad = 3
+    db.footer_pad = 3
   end
 })
