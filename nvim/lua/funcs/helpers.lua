79ceacc470d849fe12b292701685f2ff43aebc45
mccloskeybr <brendan.mccloskey@gmail.com>
2022-12-20 21:43:28 -0500

    play around with executing actions in nvim instead of using tmux

--- nvim/lua/funcs/helpers.lua
+++ nvim/lua/funcs/helpers.lua
@@ -6,6 +6,21 @@ end
 
 function get_project_root()
   local current_file = vim.fn.expand('%')
-  local project_root = vim.fn.fnamemodify(current_file, ":p:h:s?/croupier/.*?/croupier/?")
+  local project_root = vim.fn.fnamemodify(current_file, ':p:h:s?/croupier/.*?/croupier/?')
   return project_root
+end
+
+function get_build_target()
+  local current_file_dir = vim.fn.expand('%:p:h')
+  local current_file_from_google3_dir = vim.fn.fnamemodify(current_file_dir, ':s?.*google3/??')
+  local current_file_name = vim.fn.expand('%:t:r')
+  return '//' .. current_file_from_google3_dir .. ':' ..  current_file_name
+end
+
+function get_test_target()
+  local build_target = get_build_target()
+  if string.sub(build_target, -5) == '_test' then
+    return build_target
+  end
+  return build_target .. '_test'
 end
