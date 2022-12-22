7397cd7c859834206ad763ff2efaed529b79aeca
mccloskeybr <brendan.mccloskey@gmail.com>
2022-12-21 19:11:51 -0500

    add pounce highlight groups

--- nvim/after/plugin/pounce.lua
+++ nvim/after/plugin/pounce.lua
@@ -3,6 +3,10 @@ require('default.funcs.map_funcs')
 require('packer').use({
   'rlane/pounce.nvim',
   config = function()
+    vim.api.nvim_set_hl(0, 'PounceMatch', { link = 'Search' })
+    vim.api.nvim_set_hl(0, 'PounceAccept', { bg = '#648767' })
+    vim.api.nvim_set_hl(0, 'PounceAcceptBest', { bg = '#598291' })
+
     nmap('<Leader>s', '<cmd>Pounce<CR>')
     vmap('<Leader>s', '<cmd>Pounce<CR>')
     nmap('<Leader>S', '<cmd>PounceRepeat<CR>')
