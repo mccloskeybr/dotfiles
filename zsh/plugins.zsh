53556a50f0177f15a4ebad643293d66813080226
mccloskeybr <brendan.mccloskey@gmail.com>
2022-12-25 13:22:50 -0500

    zsh exec time plugin

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -11,11 +11,11 @@ zplug "plugins/python", from:oh-my-zsh
 zplug "plugins/extract", from:oh-my-zsh                 # extract compressed files
 zplug "plugins/command-not-found", from:oh-my-zsh       # suggest where missing cmd is
 zplug "plugins/colorize", from:oh-my-zsh                # syntax highlight on cat
-
 zplug "zsh-users/zsh-syntax-highlighting", defer:2      # syntax highlighting on CLI
 zplug "zsh-users/zsh-autosuggestions"                   # cmd autocomplete
 zplug "djui/alias-tips"                                 # reminds you aliases exist
 zplug "b4b4r07/enhancd", use:init.sh                    # fuzzy search for cd
+zplug "tom-auger/cmdtime"                               # show cmd exec times
 
 # install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
