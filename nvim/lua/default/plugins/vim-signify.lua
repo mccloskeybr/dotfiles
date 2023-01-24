950e4df31ae5d6b41b036475045bedaa827bba23
mccloskeybr <brendan.mccloskey@gmail.com>
2023-01-24 10:26:40 -0500

    load signify on bufreadpre for sign column

--- nvim/lua/default/plugins/vim-signify.lua
+++ nvim/lua/default/plugins/vim-signify.lua
@@ -1,9 +1,10 @@
 return {
   'mhinz/vim-signify',
+  event = 'BufReadPre',
   keys = {
     { '<Leader>sd', '<cmd>SignifyDiff<CR>' }
   },
-  config = function()
+  init = function()
     vim.g.signify_update_on_focusgained = 1
   end
 }
