18e8d645ac52f2d76e66caac1bd64ac5284ac913
mccloskeybr <brendan.mccloskey@gmail.com>
2023-03-15 12:06:15 -0400

    remove windows (was causing autocmd weirdness when invoking telescope before the animation was done), add treesitter playground

--- nvim/lua/default/plugins/treesitter.lua
+++ nvim/lua/default/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
   'nvim-treesitter/nvim-treesitter',
   -- breaking change (removal of TS* highlight groups)
   commit = '4cccb6f494eb255b32a290d37c35ca12584c74d0',
+  dependencies = {
+    'nvim-treesitter/playground'
+  },
   build = ':TSUpdate',
   event = 'BufReadPost',
   opts = {
